3f6936bc32cfc729519560a2cd70476d45806de7
Faries Tadros <ftadros1@jh.edu>
2023-09-16 19:38:07 -0400

    update

--- app.py
+++ app.py
@@ -1,40 +1,28 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import random
-import os
 
 app = Flask(__name__)
-CORS(app)
 
-# New root route
-@app.route('/')
-def root():
-    return "The app is running!"
 
-group1 = []
-group2 = []
 teams = {"Fruity Punch": [], "Bannana Split": []}
 
 @app.route('/assign_group', methods=['POST'])
 def assign_group():
-    name = request.json.get('name')
-    group = request.json.get('group')
+    data = request.json
+    name = data['name']
     
-    if group == "Group 1":
-        group1.append(name)
-    elif group == "Group 2":
-        group2.append(name)
-    else:
-        return jsonify({"error": "Invalid group"})
-        
-    all_names = group1 + group2
-    random.shuffle(all_names)
+    # Randomly assign the person to a team
+    chosen_team = random.choice(['Fruity Punch', 'Bannana Split'])
     
-    teams["Fruity Punch"] = all_names[:len(all_names)//2]
-    teams["Bannana Split"] = all_names[len(all_names)//2:]
+    # Re-balance teams if needed
+    other_team = 'Bannana Split' if chosen_team == 'Fruity Punch' else 'Fruity Punch'
+    if len(teams[chosen_team]) > len(teams[other_team]):
+        chosen_team = other_team
     
-    return jsonify({"teams": teams})
+    # Add the person to the chosen team
+    teams[chosen_team].append(name)
+    
+    return jsonify({'teams': teams})
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
