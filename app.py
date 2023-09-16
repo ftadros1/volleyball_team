000508758a5891ee46233af52986cad581f249d6
Faries Tadros <ftadros1@jh.edu>
2023-09-16 19:57:15 -0400

    latest

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import random
 import os
 
 app = Flask(__name__)
+CORS(app)
+
+# New root route
+@app.route('/')
+def root():
+    return "The app is running!"
 
 
 teams = {"Fruity Punch": [], "Bannana Split": []}
